a488b6602e59ac2a1fff6f0a5d037943c15dd0e3
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-12-10 16:38:01 -0600

    Create view-footer.php

---
+++ view-footer.php
@@ -0,0 +1,9 @@
+        <footer class="bg-light text-center text-lg-start mt-5">
+            <div class="text-center p-3" style="background-color: #f8f9fa;">
+                &copy; <?php echo date("Y"); ?> Cindy P. All rights reserved.
+            </div>
+        </footer>
+    </div> <!-- Close container -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
