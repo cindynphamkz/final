0a6294c2a74ee6fcffc84a8022aa2e370a6d3f73
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-12-10 19:52:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-header("Location: groups.php");
-exit();
+$pageTitle = "Home";
+include "view-header.php";
 ?>
+<h1>Welcome to K-pop Hub!</h1>
+<p>This website allows you to explore various K-pop groups and their albums. You can browse through group details, albums, view albums by specific groups, and even visualize data using charts. Use the navigation bar above to explore!</p>
+<?php include "view-footer.php"; ?>
