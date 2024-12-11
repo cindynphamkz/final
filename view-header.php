69e100b60cfca2a40910814640b30d0ddad1630b
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-12-10 19:52:09 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo isset($pageTitle) ? $pageTitle : 'K-pop Albums & Groups'; ?></title>
+    <title><?php echo isset($pageTitle) ? $pageTitle : 'K-pop Hub'; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -16,10 +16,19 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="view-groups.php">Groups</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="view-albums.php">Albums</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="albums-by-group.php">Albums by Group</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="groups-chart.php">Group Chart</a>
                     </li>
                 </ul>
             </div>
