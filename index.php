408e9d3628cb39eec64f5a7b12e394b5ac56fe4b
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-12-10 20:45:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -12,19 +12,19 @@ include "view-header.php";
     <!-- LE SSERAFIM -->
     <div class="col-md-4 text-center">
         <h3>LE SSERAFIM</h3>
-        <img src="https://www.billboard.com/wp-content/uploads/2023/04/LE-SSERAFIM-press-credit-SOURCE-MUSIC-2023-billboard-exclusive-1548.jpg?w=1024" class="img-fluid rounded" alt="LE SSERAFIM">
+        <img src="https://www.billboard.com/wp-content/uploads/2023/04/LE-SSERAFIM-press-credit-SOURCE-MUSIC-2023-billboard-exclusive-1548.jpg?w=1024" class="group-image" alt="LE SSERAFIM">
     </div>
 
     <!-- aespa -->
     <div class="col-md-4 text-center">
         <h3>aespa</h3>
-        <img src="https://www.kiacenter.com/assets/img/Static_TM-ArtistImage_2426x1365_Aespa_2025_National_V2-1be89b680e.jpg" class="img-fluid rounded" alt="aespa">
+        <img src="https://www.kiacenter.com/assets/img/Static_TM-ArtistImage_2426x1365_Aespa_2025_National_V2-1be89b680e.jpg" class="group-image" alt="aespa">
     </div>
 
     <!-- IVE -->
     <div class="col-md-4 text-center">
         <h3>IVE</h3>
-        <img src="https://dynamicmedia.livenationinternational.com/r/r/m/dddc9379-ee66-4dcb-b479-56ef4ba15f7e.jpg" class="img-fluid rounded" alt="IVE">
+        <img src="https://dynamicmedia.livenationinternational.com/r/r/m/dddc9379-ee66-4dcb-b479-56ef4ba15f7e.jpg" class="group-image" alt="IVE">
     </div>
 </div>
 
@@ -32,13 +32,25 @@ include "view-header.php";
     <!-- (G)I-DLE -->
     <div class="col-md-6 text-center">
         <h3>(G)I-DLE</h3>
-        <img src="https://pbs.twimg.com/media/GAPP7tFWYAAcDin?format=jpg&name=4096x4096" class="img-fluid rounded" alt="(G)I-DLE">
+        <img src="https://pbs.twimg.com/media/GAPP7tFWYAAcDin?format=jpg&name=4096x4096" class="group-image" alt="(G)I-DLE">
     </div>
 
     <!-- NMIXX -->
     <div class="col-md-6 text-center">
         <h3>NMIXX</h3>
-        <img src="https://mediaproxy.tvtropes.org/width/1200/https://static.tvtropes.org/pmwiki/pub/images/imageedit_1_2679403850.jpg" class="img-fluid rounded" alt="NMIXX">
+        <img src="https://mediaproxy.tvtropes.org/width/1200/https://static.tvtropes.org/pmwiki/pub/images/imageedit_1_2679403850.jpg" class="group-image" alt="NMIXX">
     </div>
 </div>
+
+<!-- Add inline CSS or a separate stylesheet -->
+<style>
+    .group-image {
+        width: 100%; /* Make images responsive */
+        max-width: 300px; /* Ensure consistent width */
+        height: auto; /* Maintain aspect ratio */
+        object-fit: cover; /* Ensures the image fills the frame without distortion */
+        border-radius: 10px; /* Optional: Slightly rounded corners */
+    }
+</style>
+
 <?php include "view-footer.php"; ?>
