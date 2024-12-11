d9b33358e1c16268b8bc30211b8314896f0e01de
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-12-11 01:31:44 -0600

    Update delete-album.php

--- delete-album.php
+++ delete-album.php
@@ -1,14 +1,11 @@
 <?php
 require_once("model-albums.php");
 
-// Check if the `id` parameter exists in the query string
 if (isset($_GET['id'])) {
     $albumId = $_GET['id'];
 
-    // Call the deleteAlbum function to remove the album
     deleteAlbum($albumId);
 
-    // Redirect back to the albums page
     header("Location: view-albums.php");
     exit();
 } else {
