c6f31946830385b3a0e564a532f0347cfa7788d2
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-12-10 21:48:13 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,9 +1,9 @@
-        <footer class="bg-light text-center text-lg-start mt-5">
-                <div class="footer bg-light">
-                    <p>&copy; 2024 Cindy P. All rights reserved.</p>
-                </div>
-        </footer>
-    </div> <!-- Close container -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<footer class="bg-light text-center text-lg-start mt-5">
+    <div class="footer" style="background-color: #e6d9ff; color: #000; padding: 10px 0;">
+        <p>&copy; 2024 Cindy P. All rights reserved.</p>
+    </div>
+</footer>
+</div> <!-- Close container -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
