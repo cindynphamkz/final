2ee4641280b173a88b41edb6eec557e3194c3e22
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-12-11 01:30:25 -0600

    Update add-album.php

--- add-album.php
+++ add-album.php
@@ -6,7 +6,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $albumType = $_POST['albumType'];
     $groupId = $_POST['groupid'];
 
-    // Validate inputs
     if (!empty($title) && !empty($albumType) && !empty($groupId)) {
         addAlbum($title, $albumType, $groupId);
         header("Location: view-albums.php");
