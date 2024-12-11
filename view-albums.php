9b42bd8689fa4d0ae48f599c89d7def980d696fe
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-12-10 19:53:11 -0600

    Update view-albums.php

--- view-albums.php
+++ view-albums.php
@@ -30,4 +30,37 @@ $albums = selectAlbums();
         <?php endwhile; ?>
     </tbody>
 </table>
+<!-- Add Album Modal -->
+<div class="modal fade" id="addAlbumModal" tabindex="-1" aria-labelledby="addAlbumModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form method="POST" action="add-album.php">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="addAlbumModalLabel">Add New Album</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <label for="title">Title:</label>
+                    <input type="text" id="title" name="title" class="form-control" required>
+                    <br>
+                    <label for="albumType">Album Type:</label>
+                    <input type="text" id="albumType" name="albumType" class="form-control" required>
+                    <br>
+                    <label for="groupid">Group:</label>
+                    <select id="groupid" name="groupid" class="form-control" required>
+                        <?php
+                        $groups = selectGroups();
+                        while ($group = $groups->fetch_assoc()): ?>
+                            <option value="<?php echo $group['GroupID']; ?>"><?php echo htmlspecialchars($group['Name']); ?></option>
+                        <?php endwhile; ?>
+                    </select>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Add Album</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 <?php include "view-footer.php"; ?>
