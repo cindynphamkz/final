0035dc1c2e8efa486c47bb394f45d57c9fc055d4
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-12-10 20:27:00 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -9,27 +9,26 @@
 <body>
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
-            <a class="navbar-brand" href="#">K-pop Hub</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="view-groups.php">Groups</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="view-albums.php">Albums</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="albums-by-group.php">Albums by Group</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="groups-chart.php">Group Chart</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="view-groups.php">Groups</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="view-albums.php">Albums</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="albums-by-group.php">Albums by Group</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="groups-chart.php">Group Chart</a>
+            </li>
+        </ul>
+    </div>
+</nav>
