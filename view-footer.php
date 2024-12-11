397001029f9285886cf7c9e2e9389f00a174decf
cindynphamkz <157330225+cindynphamkz@users.noreply.github.com>
2024-12-10 21:41:00 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,7 +1,7 @@
         <footer class="bg-light text-center text-lg-start mt-5">
-            <div class="text-center p-3" style="background-color: #f8f9fa;">
-                &copy; <?php echo date("Y"); ?> Cindy P. All rights reserved.
-            </div>
+                <div class="footer bg-light">
+                    <p>&copy; 2024 Cindy P. All rights reserved.</p>
+                </div>
         </footer>
     </div> <!-- Close container -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
